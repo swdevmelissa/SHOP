4ab3d02d712c9661dbddf53afaac59bbbacbba85
swdevmelissa <dev.melissa16@gmail.com>
2022-09-07 18:08:05 +0100

    login page (no validation)

---
+++ Frontend/src/screens/Login.js
@@ -0,0 +1,24 @@
+import Header from "../components/Header";
+
+const Login = () => {
+  return (
+    <>
+      <Header />
+      <div>
+        <form>
+          <input type="email" placeholder="Email" />
+          <input type="password" placeholder="Password" />
+          <button type="submit">Login</button>
+
+          <div> New costumer? 
+            <p>
+            <Link to={"/register"}> Create Account</Link>
+            </p>
+          </div>
+        </form>
+      </div>
+    </>
+  );
+};
+
+export default Login;
