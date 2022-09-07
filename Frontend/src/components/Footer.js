557b1cc018d37218f275075b1db25829e28abd30
swdevmelissa <dev.melissa16@gmail.com>
2022-09-07 17:59:18 +0100

    Footer component

---
+++ Frontend/src/components/Footer.js
@@ -0,0 +1,35 @@
+const Footer = () => {
+    return (
+      <div className="footer">
+        <div className="justify-content-center d-flex">
+          <div className="card-name">
+            <img
+              alt="mastercard"
+              src="images/footerLogo/MasterCard_Logo.png"
+            />
+          </div>
+          <div className="card-name">
+            <img
+              alt="visa"
+              src="images/footerLogo/Visa_Logo.png"
+            />
+          </div>
+          <div className="card-name">
+            <img
+              alt="paypal"
+              src="images/footerLogo/Paypal_Logo.png"
+            />
+          </div>
+          <div className="card-name">
+            <img
+              alt="express"
+              src="images/footerLogo/AmericanExpress_Logo.png"
+            />
+          </div>
+        
+        </div>
+      </div>
+    );
+  };
+  
+  export default Footer;
--- Frontend/src/screens/HomeScreen.js
+++ Frontend/src/screens/HomeScreen.js
@@ -1,10 +1,10 @@
 import Header from "../components/Header";
-
+import Footer from "../components/Footer";
 const HomeScreen = () => {
     return (
       <div>
         <Header />
-        
+        <Footer />
       </div>
     )
 }
