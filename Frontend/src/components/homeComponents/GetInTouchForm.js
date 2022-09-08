1a519d918c317f26b2fd4c67a261a5fb52e3cebf
swdevmelissa <dev.melissa16@gmail.com>
2022-09-08 13:01:28 +0100

    getInTouch Section + style

---
+++ Frontend/src/components/homeComponents/GetInTouchForm.js
@@ -0,0 +1,26 @@
+const GetInTouchForm = () => {
+  return (
+    <div className="subscribe-section bg-with-black">
+      <div className="container">
+        <div className="row">
+          <div className="col-xs-12">
+            <div className="subscribe-head">
+              <h2> Do you need more tips?</h2>
+              <p> Get in touch with us and get the latest tips.</p>
+              <form className="form-section">
+                <input type="email" name="email" placeholder="Your Email.." />
+                <input
+                  type="submit"
+                  name="subscribe"
+                  value="Subscribe"
+                />
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default GetInTouchForm;
--- Frontend/src/screens/HomeScreen.js
+++ Frontend/src/screens/HomeScreen.js
@@ -1,12 +1,14 @@
 import Header from "../components/Header";
 import Footer from "../components/Footer";
 import ProductSection from '../components/homeComponents/ProductSection';
+import GetInTouchForm from "../components/homeComponents/GetInTouchForm";
 
 const HomeScreen = () => {
     return (
       <div>
         <Header />
         <ProductSection />
+        <GetInTouchForm />
         <Footer />
       </div>
     )
